26f3de9b50c6cb8e14fa587554e5d196ff9d57f9
nickgiki <116651474+nickgiki@users.noreply.github.com>
2026-09-07 17:43:29 +0300

    fixing conceptual bug

--- src/codelexity/fte_calculations.py
+++ src/codelexity/fte_calculations.py
@@ -7,8 +7,9 @@ COCOMO_MODES = {
 
 
 def maintenance_effort_ftes(total_lines: int, act=0.1, score: float = 0.0):
-    kloc = act * total_lines / 1000
+    kloc = total_lines / 1000
     a_opt, b_opt = COCOMO_MODES["organic"]
     a_pess, b_pess = COCOMO_MODES["embedded"]
-    low, high = round((a_opt * kloc**b_opt) / 12, 1), round((a_pess * kloc**b_pess) / 12, 1)
+    low = round(act * (a_opt * kloc**b_opt) / 12, 1)
+    high = round(act * (a_pess * kloc**b_pess) / 12, 1)
     return (low, round(low + score * (high - low), 1), high)
